f16c350cd9d855c6ff04ccc415d372b28d580d37
Aaron Lav <asl2@pobox.com>
2003-12-08 22:25:10 +0000

    An example of the '+' syntax for searching multiple dbs at once

---
+++ example/multdb.py
@@ -0,0 +1,9 @@
+from PyZ3950 import zoom
+
+conn = zoom.Connection('www.lib.csu.ru', 210)
+conn.databaseName = 'arefd+knigi+liter+period'
+conn.preferredRecordSyntax = 'USMARC'
+query = zoom.Query ('CCL', 'ti=Journal or au=Turgenev')
+res = conn.search (query)
+for r in res:
+    print "db:", repr(r.databaseName), r
