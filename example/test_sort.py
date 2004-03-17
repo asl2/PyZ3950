c1bee5b550a476d5eebaae861a142d5a0e2a9784
Rob Sanderson <azaroth42@gmail.com>
2004-03-17 12:47:44 +0000

    Sort operation testing code

---
+++ example/test_sort.py
@@ -0,0 +1,26 @@
+#!/usr/local/bin/python2.3 -i
+
+from zoom import *
+c = Connection('gondolin.hist.liv.ac.uk', 210)
+c.databaseName = 'l5r'
+#c = Connection('z3950.copac.ac.uk', 210)
+#c.databaseName = 'COPAC'
+c.preferredRecordSyntax = 'SUTRS'
+q = Query('pqf', '@attr 1=4 "sword"')
+q2 = Query('pqf', '@attr 1=3201 foo')
+rs = c.search(q)
+print len(rs)
+print rs[0].data
+sk = SortKey()
+
+#sk.sequence = q2
+
+sk.type = "private"
+sk.sequence = "/card/name"
+sk.relation = "descending"
+
+#sk.sequence = "rank"
+#sk.type = "elementSetName"
+
+rs2 = c.sort([rs], [sk])
+print rs2[0].data
