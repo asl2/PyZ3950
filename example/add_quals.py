33c82e9e15982fad03ac42a8b50d981febcd01a5
Aaron Lav <asl2@pobox.com>
2004-07-20 17:21:44 +0000

    *** empty log message ***

---
+++ example/add_quals.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Demonstrates adding qualifiers at runtime by mutating ccl.qual_dict,
+and calling ccl.relex ()."""
+
+from PyZ3950 import zoom, ccl
+
+ccl.add_qual ('AUPERSONAL', (1,1))
+
+conn = zoom.Connection ('z3950.loc.gov', 7090)
+conn.databaseName = 'VOYAGER'
+conn.preferredRecordSyntax = 'USMARC'
+
+query = zoom.Query ('CCL', 'aupersonal=MacLane, Saunders')
+
+res = conn.search (query)
+for r in res[:20]:
+    print r
+
+conn.close ()
+
