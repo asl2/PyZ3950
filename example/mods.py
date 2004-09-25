4d5348c8c8a3dbdef967bc3ef0d35dff2a555074
Aaron Lav <asl2@pobox.com>
2004-09-25 15:51:39 +0000

    *** empty log message ***

---
+++ example/mods.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from PyZ3950 import zoom, zmarc
+
+def make_conn ():
+    conn = zoom.Connection ('z3950.loc.gov', 7090)
+    conn.databaseName = 'VOYAGER'
+    conn.preferredRecordSyntax = 'USMARC'
+    return conn
+
+
+def fetch_mods (query):
+    res = conn.search (query)
+    mods_list = []
+    for r in res:
+        marc_obj = zmarc.MARC (r.data)
+        mods_list.append (marc_obj.toMODS ())
+    return mods_list
+
+conn = make_conn ()
+mods_list = fetch_mods (zoom.Query ('CCL', 'ti="1066 and all that"'))
+print mods_list
+
+
+    
