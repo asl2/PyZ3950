59c3b371752097bfecedc9f55e59ad0808d7a3d7
Aaron Lav <asl2@pobox.com>
2012-02-12 16:46:06 -0600

    Add UNIMARC for Louis (spalax@gresille.org)

--- PyZ3950/zoom.py
+++ PyZ3950/zoom.py
@@ -806,6 +806,8 @@ _RecordType ('USMARCnonstrict', z3950.Z3950_RECSYN_USMARC_ov,
             renderer = lambda v: str(zmarc.MARC(v, strict=0)))
 _RecordType ('UKMARC', z3950.Z3950_RECSYN_UKMARC_ov,
             renderer = lambda v: str(zmarc.MARC(v)))
+_RecordType ('UNIMARC', z3950.Z3950_RECSYN_UNIMARC_ov,
+            renderer = lambda v: str(zmarc.MARC(v))
 _RecordType ('SUTRS', z3950.Z3950_RECSYN_SUTRS_ov)
 _RecordType ('XML', z3950.Z3950_RECSYN_MIME_XML_ov)
 _RecordType ('SGML', z3950.Z3950_RECSYN_MIME_SGML_ov)
