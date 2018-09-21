40379f66411ac08e8cb4303c311c1e4f828c79ce
Dan Davis <dan@danizen.net>
2018-09-20 21:08:07 -0400

    Turn test/test1.py into our first python test
    - The ability to run it as an interactive script is preserved

--- test/test1.py
+++ test/test1.py
@@ -1,15 +1,33 @@
 #!/usr/bin/env python
 from __future__ import print_function, absolute_import
 from PyZ3950 import zoom
+from io import StringIO
 
-conn = zoom.Connection ('z3950.loc.gov', 7090)
-conn.databaseName = 'VOYAGER'
-conn.preferredRecordSyntax = 'USMARC'
 
-query = zoom.Query ('CCL', 'ti="1066 and all that"')
+def get_results():
+    conn = zoom.Connection ('z3950.loc.gov', 7090)
+    conn.databaseName = 'VOYAGER'
+    conn.preferredRecordSyntax = 'USMARC'
 
-res = conn.search (query)
-for r in res:
-    print(r)
-conn.close ()
+    query = zoom.Query ('CCL', 'ti="1066 and all that"')
 
+    res = conn.search (query)
+    for r in res:
+        yield(r)
+    conn.close ()
+
+
+def test_1():
+    results = list(get_results())
+    assert len(results) == 13
+
+    
+
+
+def main():
+    for r in get_results():
+        print(r)
+
+
+if __name__ == '__main__':
+    main()
